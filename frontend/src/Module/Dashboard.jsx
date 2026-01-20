a38491a6a0d1ecf23bac53c5f64f7664f1e93d29
JhonnBretana <jhonnbretz@gmail.com>
2026-01-20 22:49:54 +0800

    bg fix dark

--- frontend/src/Module/Dashboard.jsx
+++ frontend/src/Module/Dashboard.jsx
@@ -4,9 +4,9 @@ import Header from "@/Components/Layout/Header";
 function Dashboard() {
   return (
     <>
-      <div style={{ width: "100%", height: "100vh", position: "relative", backgroundColor: "#0a0a0a" }}>
+      <div style={{ width: "100%", height: "100vh", position: "relative", backgroundColor: "#0a0a0a" }} className="dark">
         
-        <div style={{ position: "absolute", width: "100%", height: "100%", top: 0, left: 0, filter: "invert(1) brightness(0.9)" }}>
+        <div style={{ position: "absolute", width: "100%", height: "100%", top: 0, left: 0}}>
           <HexagonBackground />
         </div>
           <Header/>
